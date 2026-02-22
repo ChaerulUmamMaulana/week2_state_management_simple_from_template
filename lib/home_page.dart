e93ee1a07abfee8f3d8d5dab6e0006cfbbc8a27a
chaerulumammaulana <1123150132@global.ac.id>
2026-02-22 07:59:10 -0800

    update home_page.dart pada tampilan text counter

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -65,7 +65,7 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
                 const Text('You have pushed the button this many times:'),
                 Text(
-                  '$_counter',
+                  value.angka.toString(),
                   style: Theme.of(context).textTheme.headlineMedium,
                 ),
               ],
