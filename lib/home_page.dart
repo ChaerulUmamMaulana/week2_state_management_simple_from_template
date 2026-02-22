8a7feb499e1d0581904b1eb5e0fc1f0b4d57ee36
chaerulumammaulana <1123150132@global.ac.id>
2026-02-22 07:57:16 -0800

    Wraping Consumer dari library provider

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:state_management/counter_model.dart';
+import 'package:provider/provider.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -29,8 +32,8 @@ class _MyHomePageState extends State<MyHomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
-    return Builder(
-      builder: (context) {
+    return Consumer<CounterModel>(
+      builder: (context, value, child) {
         return Scaffold(
           appBar: AppBar(
             // TRY THIS: Try changing the color here to a specific color (to
